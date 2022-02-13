1406f11b24d20444af10e681a71231cb61a71a30
Praveen Jasso <praveen.jasso91@gmail.com>
2022-02-13 15:42:24 +0100

    Hello World with status 200 on GET

---
+++ src/test/java/com/qvik/MessageLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qvik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
